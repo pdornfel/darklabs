088f0140cec25f2c946e0675e3d3daba70d47b82
Paul Dornfeld <pdornfel@gmail.com>
2015-04-04 17:36:01 -0400

    coming soon

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,27 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require jquery-ui
+//= require pulse
 //= require_tree .
+
+
+
+$(document).ready(function(){
+
+  var properties = {
+     fontSize: "15px",
+     paddingTop: '0px',
+     paddingBottom: '0px'
+  };
+
+  var el1 = $('#coming-soon #first');
+  var el2 = $('#coming-soon #second');
+  var el3 = $('#coming-soon #third');
+
+  el1.delay(50).pulse(properties, {duration : 1500, pulses: 5000});
+  el2.delay(1000).pulse(properties, {duration : 1500, pulses: 5000});
+  el3.delay(1500).pulse(properties, {duration : 1500, pulses: 5000});
+
+
+})
